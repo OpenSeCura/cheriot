6de7a170048d6918dda27a570fea388ed45ed1cd
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2026-08-08 16:21:10 -0700

    clamping base to 0 and 2^32

--- SpecDefines.v
+++ SpecDefines.v
@@ -642,8 +642,11 @@ Section CapEncoding.
         LetE aTop: Bit (AddrSz - CapBSz) <- TruncMsb (AddrSz - CapBSz) CapBSz #aMidTop;
 
         LetE aHi <- ZeroExtendTo (AddrSz - CapBSz) (ToBit (Slt #aMid #B));
-        LetE aTopB <- Sub #aTop #aHi;
-        LetE base <- Sll (ZeroExtendTo (AddrSz + 1) ({< #aTopB, #B >})) #ECorrected;
+        LetE aTopB <- ITE0 (isNotZero #aTop) (Sub #aTop #aHi);
+        LetE baseInit <- Sll (ZeroExtendTo (AddrSz + 1) ({< #aTopB, #B >})) #ECorrected;
+        LetE base <- ITE (And [isNotZero (TruncMsb 1 (CapBSz-1) #B); Eq #ECorrected $Emax])
+                       $(Z.pow 2 Xlen)
+                       #baseInit;
 
         LetE tHi <- ZeroExtendTo (AddrSz - CapBSz) (ToBit (Slt #T #B));
         LetE aTopT <- Add [#aTopB; #tHi];
