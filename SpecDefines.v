f57e828f61e08feb8fa8633b52985ead4dcd3f18
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2026-09-11 18:20:33 -0700

    added roots

--- SpecDefines.v
+++ SpecDefines.v
@@ -703,6 +703,72 @@ Section CapEncoding.
   End BaseTop.
 End CapEncoding.
 
+Section Roots.
+  Definition ExecRootPerms : type CapPerms := (STRUCT_CONST {
+                                                   "U0" ::= false;
+                                                   "SE" ::= false;
+                                                   "US" ::= false;
+                                                   "EX" ::= true;
+                                                   "SR" ::= true;
+                                                   "MC" ::= true;
+                                                   "LD" ::= true;
+                                                   "SL" ::= false;
+                                                   "LM" ::= true;
+                                                   "SD" ::= false;
+                                                   "LG" ::= true;
+                                                   "GL" ::= true }).
+
+  Definition MemRootPerms : type CapPerms := (STRUCT_CONST {
+                                                  "U0" ::= false;
+                                                  "SE" ::= false;
+                                                  "US" ::= false;
+                                                  "EX" ::= false;
+                                                  "SR" ::= false;
+                                                  "MC" ::= true;
+                                                  "LD" ::= true;
+                                                  "SL" ::= true;
+                                                  "LM" ::= true;
+                                                  "SD" ::= true;
+                                                  "LG" ::= true;
+                                                  "GL" ::= true }).
+
+  Definition SealRootPerms : type CapPerms := (STRUCT_CONST {
+                                                   "U0" ::= true;
+                                                   "SE" ::= true;
+                                                   "US" ::= true;
+                                                   "EX" ::= false;
+                                                   "SR" ::= false;
+                                                   "MC" ::= false;
+                                                   "LD" ::= false;
+                                                   "SL" ::= false;
+                                                   "LM" ::= false;
+                                                   "SD" ::= false;
+                                                   "LG" ::= false;
+                                                   "GL" ::= true }).
+
+  Section CreateRoot.
+    Variable perms: type CapPerms.
+    Definition createRootECap: type ECap :=
+      (STRUCT_CONST {
+           "R" ::= false ;
+           "perms" ::= perms ;
+           "oType" ::= getDefault (Bit _) ;
+           "cE" ::= Zmod.of_Z _ Emax ;
+           "top" ::= Zmod.app (Zmod.zero: bits AddrSz) Zmod.one ;
+           "base" ::= Zmod.zero }).
+
+    Definition createRootECap (addr: Z): type FullECapWithTag :=
+      (STRUCT_CONST {
+           "tag" ::= true;
+           "ecap" ::= createRootECap;
+           "addr" ::= Zmod.of_Z _ addr }).
+  End CreateRoot.
+
+  Definition ExecRootECap (addr: Z) := createRootECap ExecRootPerms addr.
+  Definition MemRootECap (addr: Z) := createRootECap MemRootPerms addr.
+  Definition SealRootECap (addr: Z) := createRootECap SealRootPerms addr.
+End Roots.
+
 Definition isSealed ty (ecap: ty ECap) : Expr ty Bool := isNotZero (##ecap`"oType").
 Definition isSealingCap ty (ecap : ty ECap) : Expr ty Bool :=
   Or [ ##ecap`"perms"`"SE" ;
