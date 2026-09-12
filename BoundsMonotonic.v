e6dc45c9668466ebe8bd4a3e40d409f98189f989
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2026-09-12 12:59:44 -0700

    moved evalLetPropGen to Guru

--- BoundsMonotonic.v
+++ BoundsMonotonic.v
@@ -1,5 +1,5 @@
 From Stdlib Require Import List String Ascii ZArith Znumtheory Zmod Zmod.Bits Lia Bool.
-From Guru Require Import Library Syntax Semantics Notations MergeFold.
+From Guru Require Import Library Syntax Semantics Notations MergeFold Theorems.
 From Cheriot Require Import SpecDefines FunctionalUnits.
 
 Set Implicit Arguments.
@@ -537,32 +537,6 @@ Proof.
   change (@Zmod.Private_to_Z ?m) with (@Zmod.unsigned m).
   rewrite cE_decode_id by exact Hne.
   reflexivity.
-Qed.
-
-Fixpoint evalLetPropGen {k} (le: LetExpr type k) (P: type k -> Prop) : Prop :=
-  match le with
-  | RetE e => P (evalExpr e)
-  | SystemE ls cont => evalLetPropGen cont P
-  | LetEx s k' le cont => forall (res : type k'), res = evalLetExpr le -> evalLetPropGen (cont res) P
-  | IfElseE s p k' t f cont =>
-      if evalExpr p then forall (res : type k'), res = evalLetExpr t -> evalLetPropGen (cont res) P
-                    else forall (res : type k'), res = evalLetExpr f -> evalLetPropGen (cont res) P
-  end.
-
-Lemma evalLetPropGen_sound :
-  forall {k} (le: LetExpr type k) P,
-    evalLetPropGen le P -> P (evalLetExpr le).
-Proof.
-  fix evalLetPropGen_sound 2.
-  intros k le P H.
-  destruct le as [e | ls cont | s k' le1 cont | s p k' t f cont].
-  - exact H.
-  - apply (evalLetPropGen_sound _ cont P H).
-  - apply (evalLetPropGen_sound _ (cont (evalLetExpr le1)) P (H (evalLetExpr le1) eq_refl)).
-  - simpl in H. simpl.
-    destruct (evalExpr p).
-    + apply (evalLetPropGen_sound _ (cont (evalLetExpr t)) P (H (evalLetExpr t) eq_refl)).
-    + apply (evalLetPropGen_sound _ (cont (evalLetExpr f)) P (H (evalLetExpr f) eq_refl)).
 Qed.
 
 (* ========================================================================= *)
